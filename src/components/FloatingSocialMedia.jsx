7a0471e0bdd3ee7b2ba1c902958bfd18591a6c61
Akash <goravanakollakash@gmail.com>
2024-12-12 15:02:40 +0530

    changed social media links

--- src/components/FloatingSocialMedia.jsx
+++ src/components/FloatingSocialMedia.jsx
@@ -13,7 +13,7 @@ const FloatingSocialMedia = () => {
       </a>
       {/* Facebook */}
       <a
-        href="https://facebook.com"
+        href="https://www.facebook.com/share/19rchZYxBw/"
         target="_blank"
         rel="noopener noreferrer"
         className="flex items-center justify-center w-12 h-12 bg-blue-600 text-white rounded-full shadow-lg hover:bg-blue-700 transition z-1"
@@ -23,7 +23,7 @@ const FloatingSocialMedia = () => {
 
       {/* Instagram */}
       <a
-        href="https://instagram.com"
+        href="https://www.instagram.com/krishnastayinn?igsh=MThzY2ppa3VmNXJqMQ=="
         target="_blank"
         rel="noopener noreferrer"
         className="flex items-center justify-center w-12 h-12 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-white rounded-full shadow-lg hover:opacity-90 transition"
@@ -33,7 +33,7 @@ const FloatingSocialMedia = () => {
 
       {/* WhatsApp */}
       <a
-        href="https://wa.me/yourwhatsapplink"
+        href="https://wa.me/919972014954"
         target="_blank"
         rel="noopener noreferrer"
         className="flex items-center justify-center w-12 h-12 bg-green-500 text-white rounded-full shadow-lg hover:bg-green-600 transition"
